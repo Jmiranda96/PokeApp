6d1b17a7d5447620212fbaa57c0791b6f78bdc77
Jorge Miranda <j.miranda@druo.com>
2020-08-26 13:46:30 -0500

    Initial Commit

---
+++ PokeApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PokeApp
+//
+//  Created by Jorge Miranda on 26/08/20.
+//  Copyright © 2020 Jorge Miranda. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
