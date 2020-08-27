be9950c88133b4090ef81853f7f51e8c6b42c740
Jorge Miranda <j.miranda@druo.com>
2020-08-27 14:54:42 -0500

    PokemonDetails view and controller created

---
+++ PokeApp/Details/PokemonDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PokemonDetailsViewController.swift
+//  PokeApp
+//
+//  Created by Jorge Miranda on 27/08/20.
+//  Copyright © 2020 Jorge Miranda. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailsViewController: UIViewController {
+
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var mainLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
