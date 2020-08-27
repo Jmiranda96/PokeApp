d5fc616851e4e2b07ac59a666d95fd61ddfd9595
Jorge Miranda <j.miranda@druo.com>
2020-08-26 22:55:32 -0500

    feat: importation of PokemonAPI in podfiles and creation of PokemonModel

---
+++ PokeApp/Pokemon/PokemonModel.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonModel.swift
+//  PokeApp
+//
+//  Created by Jorge Miranda on 26/08/20.
+//  Copyright © 2020 Jorge Miranda. All rights reserved.
+//
+
+import Foundation
+import PokemonAPI
+
+
+class PokemonModel {
+    
+    
+    
+}
